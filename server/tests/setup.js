c2abb59bbbeb60eae38df27bd7ec22749765fda3
hxddad <haddad.yazan117@gmail.com>
2026-06-03 13:56:19 -0400

    fix: ensure JWT_SECRET is set during test setup

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -7,6 +7,7 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const uri = mongoServer.getUri();
   process.env.MONGO_URI = uri;
+  process.env.JWT_SECRET = process.env.JWT_SECRET || 'testsecret';
   await mongoose.connect(uri);
 });
 
