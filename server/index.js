aef70808bd376bec22124af365ecd22c29262f93
hxddad <haddad.yazan117@gmail.com>
2026-06-04 13:20:51 -0400

    added socket.io and socket.io client

--- server/index.js
+++ server/index.js
@@ -3,18 +3,35 @@ import cors from "cors";
 import env from "dotenv";
 import authRoutes from "./api/auth.js";
 import mongoose from "mongoose";
+import { createServer } from 'node:http';
 import { setServers } from "dns";
+import { Server } from 'socket.io';
 
 setServers(['8.8.8.8', '1.1.1.1']);
 
 env.config();
 
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.use(express.json());
 
@@ -26,7 +43,7 @@ if (process.env.NODE_ENV !== "test") {
     .then(() => {
       console.log("MongoDB connected");
 
-      app.listen(process.env.PORT, () => {
+      server.listen(process.env.PORT, () => {
         console.log(`Server running on port ${process.env.PORT}`);
       });
     })
--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 import './Chat.css'
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000", {
+  withCredentials: true,
+});
+
+socket.on("connect", () => {
+  console.log("Connected:", socket.id);
+});
 const messages = [
   {
     author: 'Yazan',
