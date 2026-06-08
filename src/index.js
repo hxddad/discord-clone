f2e2c927426006770c08899b4cfc16f899e93b92
hxddad <haddad.yazan117@gmail.com>
2026-06-08 17:16:41 -0400

    move server folder to src

--- src/index.js
+++ src/index.js
@@ -14,24 +14,10 @@ env.config();
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    credentials: true,
-  },
-});
-
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
 
 app.use(express.json());
 
